3daa0465be09773fd6f50a55a0ff9feb953bbf4e
Mike Kemakolam <mikekemakolam@MBPM2-HGKM633XMW-KEMAKOLAM.local>
2024-05-24 14:18:24 -0400

    add functionality to ensure proper text displayed appropriate text on button click

--- app/src/main/java/com/example/ui_testing/MainActivity.kt
+++ app/src/main/java/com/example/ui_testing/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.example.ui_testing
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //create references for each component on screen
+        val button = findViewById<ExtendedFloatingActionButton>(R.id.efabPrintGreeting)
+        val enteredValue = findViewById<EditText>(R.id.etEnterName)
+        val display = findViewById<TextView>(R.id.tvDisplayResult)
+
+        //add functionality
+        button.setOnClickListener {
+            if(enteredValue.text.isNotEmpty()){
+                display.text = "Hello ${enteredValue.text} hope you're having a great day"
+            }else{ display.text = "Please enter in a valid name"
+
+            }
+        }
     }
 }
